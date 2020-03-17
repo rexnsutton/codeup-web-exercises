547ff34cc6559a41a4dba5ae70d773f2ceaa5cd2
Student Name <rexsutton2@gmail.cogit>
2020-03-17 15:49:54 -0500

    started finctions exercise

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,16 @@
 "use strict";
 
-var myNameIs = 'Bob'; // TODO: Fill in your name here.
+var myNameIs = 'Rex'; // TODO: Fill in your name here.
 
 // TODO:
 // Create a function called 'sayHello' that takes a parameter 'name'.
 // When called, the function should log a message that says hello from the passed in name.
+function sayHello(name) {
+    console.log('Hello from', name);
+}
 
 // TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+sayHello(myNameIs);
 
 // Don't modify the following line
 // It generates a random number between 1 and 100 and stores it in random
@@ -14,7 +18,12 @@ var random = Math.floor((Math.random()*100)+1);
 
 // TODO:
 // Create a function called 'isOdd' that takes a number as a parameter.
+function isOdd(number) {
+    console.log('this number is odd' ||  number % 2 = 1);
+}
+
 // The function should use the ternary operator to log a message.
 // The log should tell the number passed in and whether it is odd or not.
 
 // TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
+isOdd(random);
