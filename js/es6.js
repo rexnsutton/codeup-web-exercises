c2520374bdb1ada09982e2456ce2409fbb22d058
Student Name <rexsutton2@gmail.cogit>
2020-04-22 09:35:05 -0500

    final commit

--- js/es6.js
+++ js/es6.js
@@ -72,22 +72,21 @@ users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
+  const {name, email, languages} = user;
 
-  const developers = {name: 'Rex', email: 'rexsutton2@gmail.com'}
   // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+  developers.push(`${name}'s email is ${email}  ${name}  knows ${languages.join(', ')}`);
 });
 
 // TODO: Use `let` for the following variable
-var list = '<ul>';
+let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
-
+// developers.forEach(function (developer) {
+for(let developer of developers){
+  list += `<li>${developer}</li>`;
+}
   // TODO: rewrite the assignment below to use template strings
-  list += '<li>' + developer + '</li>';
-});
-list += '</ul>';
+//   list += '<li>  </li>';
+// });
+// list += '</ul>';
