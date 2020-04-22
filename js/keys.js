ab6142a34c5cb71aa5939f58953e7885c1272a3e
Student Name <rexsutton2@gmail.cogit>
2020-04-22 17:20:43 -0500

    rename

---
+++ js/keys.js
@@ -0,0 +1,3 @@
+const mapboxtoken = 'pk.eyJ1IjoicmV4c3V0dG9uMiIsImEiOiJjazhqODQ1emwwMW03M25vNjF6b3F0MDZuIn0.dpnGJHqcUfHg4BDqpRCZ-g';
+const OWM_KEY = '46bea0a3eac1d0c6863004b49c253484';
+const GITHUB_KEY = '6421b3caaaec91d626c07491ec7e6c1b746f34ad';
